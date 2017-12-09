66154e92383eccb0d2ff96f22c72635cc95b9653
Frank Huang <xih168@ucsd.edu>
2017-12-08 21:13:28 -0800

    Add: forget password and confirm password,minifest updated

--- app/src/main/java/com/example/stevetran/pantryraider/Login/LoginActivity.java
+++ app/src/main/java/com/example/stevetran/pantryraider/Login/LoginActivity.java
@@ -73,8 +73,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void forgetPressed(View view) {
-        Intent forgetPassword = new Intent(this, ForgetPasswordActivity.class);
-        startActivity(forgetPassword);
+        Intent intent = new Intent(this, ForgetPasswordActivity.class);
+        startActivity(intent);
+        return;
     }
 
     @Override
